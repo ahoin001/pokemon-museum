43bc53dc5e2a214749d3e04c1e03bc8bce34b6d5
Alexander Hoinville <ahoin001@gmail.com>
2020-04-29 00:11:11 -0400

    Updates

---
+++ precache-manifest.a2b032cf8cec275c824c5fbeb672f47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662774593d85f788291b96320c16ad7c",
+    "url": "/pokemon-museum/index.html"
+  },
+  {
+    "revision": "780a1dc45d00c47b5cd9",
+    "url": "/pokemon-museum/static/css/main.c5a5346f.chunk.css"
+  },
+  {
+    "revision": "cf9e5330c81e722e5aad",
+    "url": "/pokemon-museum/static/js/2.cf8e2cad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-museum/static/js/2.cf8e2cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780a1dc45d00c47b5cd9",
+    "url": "/pokemon-museum/static/js/main.1b816623.chunk.js"
+  },
+  {
+    "revision": "05473e888cb71231d5f1",
+    "url": "/pokemon-museum/static/js/runtime-main.742d91cc.js"
+  }
+]);
